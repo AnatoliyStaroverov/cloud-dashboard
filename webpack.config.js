8e3ff92a5fb72e86ed430e4b9bc480b99269388e
AnatoliyStaroverov <Anatoliy.Staroverov1234@gmail.com>
2020-07-09 22:38:41 -0700

    Init docker build.

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,14 @@ module.exports = {
       }
     ]
   },
+  // The point or points to enter the application. This is where Webpack will
+  // start. We generally have one entry point per HTML page. For single-page
+  // applications, this means one entry point. For traditional multi-page apps,
+  // we may have multiple entry points.
+  // https://webpack.js.org/concepts#entry
+  entry: [
+    path.join(PATH_SOURCE, './index.js'),
+  ],
   plugins: [
     new HtmlWebPackPlugin({
       template: "./src/index.html",
